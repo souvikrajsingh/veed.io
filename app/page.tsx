43b106facf660980cf0671a6e88d926eb669c261
souvikrajsingh <souvikrajsingh@gmail.com>
2024-05-26 15:19:59 +0530

    intitial-commit-dropzones

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
-import { Welcome } from "../components/Welcome/Welcome";
+"use client";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button } from "@mantine/core";
+import React, { useEffect } from "react";
+
 import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
+import { DropzoneButton } from "../components/Dropzones/DropzoneButton";
+import { DoubleNavbar } from "../components/DoubleNavbar/DoubleNavbar";
 
 export default function HomePage() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  useEffect(() => {
+    open();
+  }, [open]);
+
+  const handleFileUpload = (file: File) => {
+    // Handle the uploaded file here
+    console.log(file);
+  };
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <DoubleNavbar />
+      {/* 
+      <Modal
+      opened={opened}
+      onClose={close}
+      title="Enter a Audio file "
+      centered
+    >
+      <DropzoneButton />
+    </Modal>
+
+    <ColorSchemeToggle /> */}
     </>
   );
 }
